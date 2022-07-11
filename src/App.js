510b59055a5f7fa491a6cd4486bdef6f8ad5e2d4
khumbulani <kerkhood9@gmail.com>
2022-07-11 11:58:52 +0200

    app.js

--- src/App.js
+++ src/App.js
@@ -4,8 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-     
-     <div><a  src="http://localhost:3000/" title='Go home'></a></div>
+     <Speech/>
     </div>
   );
 }
